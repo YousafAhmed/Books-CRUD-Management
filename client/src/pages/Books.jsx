81e352d136e88461b4ce24611c5b414690241548
YousafAhmed <yousafahmed537@gmail.com>
2023-09-26 15:38:15 +0500

    modified:   client/node_modules/.cache/.eslintcache 	modified:   client/node_modules/.cache/default-development/0.pack 	modified:   client/node_modules/.cache/default-development/3.pack 	new file:   client/node_modules/.cache/default-development/4.pack 	modified:   client/node_modules/.cache/default-development/index.pack 	modified:   client/node_modules/.cache/default-development/index.pack.old 	modified:   client/src/pages/Books.jsx

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
 const Books = () =>{
+
+    const [books,setBooks] = useState([])
+
+    useEffect(()=>{
+        const fecthAllBooks = async()=>{
+    try{
+        const res = await axios.get("http:localhost:8800/books")
+        console.log(res)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+fetchAllBooks()
+},[])
+
     return(
         <div>Books</div>
     )
